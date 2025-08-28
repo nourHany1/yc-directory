3056fe52171f3cf518338a2c8e8faa9d2aba532e
nourHany1 <162379890+nourHany1@users.noreply.github.com>
2025-08-28 06:57:23 +0300

    feat: first commit

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,26 @@ const nextConfig: NextConfig = {
     config.ignoreWarnings = [/failed to load source map/];
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const sentryConfig = {
